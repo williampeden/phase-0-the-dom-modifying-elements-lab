d3561282deb6a2a9099bbead7f8c180608123291
William <wgepeden@gmail.com>
2022-02-01 21:37:14 -0500

    commit tree 331a0e8e1869a5f359357c830d1b150840c7e229

--- index.js
+++ index.js
@@ -1,1 +1,6 @@
-// Write your code here!
+main.remove();
+const newHeader = document.createElement('h1')
+newHeader.id = 'victory'
+
+newHeader.textContent = "William is the champion"
+document.body.append(newHeader);
